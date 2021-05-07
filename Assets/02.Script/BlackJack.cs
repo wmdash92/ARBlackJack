f86c9273e02da5f6078dcf4834dc52f0db72c55f
wmdash92 <wmdash92@gmail.com>
2021-05-07 14:17:45 +0900

    Lobby Scene1

--- Assets/02.Script/BlackJack.cs
+++ Assets/02.Script/BlackJack.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class BlackJack : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
+
+
     void Update()
     {
         
     }
+
+
+
 }
--- Assets/02.Script/PhotonManager.cs
+++ Assets/02.Script/PhotonManager.cs
@@ -1,17 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+using UnityEngine.UI;
 
-public class PhotonManager : MonoBehaviour
+
+
+
+
+public class PhotonManager : MonoBehaviourPunCallbacks
 {
 
 
 
+    private readonly string gameVersion = "v1.0";
+    private string userId = "User_07";
+
+    public TMP_InputField roomNameText;
 
 
-    // Start is called before the first frame update
+
+
+
+
+
+
+
+
+
+
+
+    void Awake()
+    {
+
+
+        PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.GameVersion = gameVersion;
+        
+
+        PhotonNetwork.ConnectUsingSettings();
+
+
+    }
+
+
+
     void Start()
     {
+        userId = PlayerPrefs.GetString("USER_ID", $"USER_{Random.Range(0,100) :00}");
         
     }
 
@@ -20,11 +58,80 @@ public class PhotonManager : MonoBehaviour
 
 
 
-    // Update is called once per frame
-    void Update()
+
+
+    public override void OnConnectedToMaster()
     {
-        
+        Debug.Log("서버 접속!!");
+
+        PhotonNetwork.JoinLobby();
+
     }
+
+
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+        {
+            Debug.Log("로비 접속!!");
+        }
+    }
+
+
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log($"code={returnCode}, msg ={message}");
+        
+        RoomOptions ro = new RoomOptions();
+        ro.IsOpen =true;
+        ro.IsVisible = true;
+        ro.MaxPlayers = 4;
+
+
+        if (string.IsNullOrEmpty(roomNameText.text))
+        {
+            roomNameText.text = $"ROOM_{Random.Range(1,100):000}";
+        }
+        
+        PhotonNetwork.CreateRoom(roomNameText.text, ro);
+    }
+
+
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("방 생성!!");
+    }
+
+
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("방 접속!!");
+        // Debug.Log(PhotonNetwork.CurrentRoom.Name);
+
+
+        // if(PhotonNetwork.IsMasterClient)
+        // {
+
+        PhotonNetwork.LoadLevel("PlayScene");
+        // }
+
+    }
+
+
+
+    public void OnLoginClick()
+    {
+
+        PhotonNetwork.CreateRoom(roomNameText.text);
+
+        PhotonNetwork.JoinRandomRoom();
+
+    }
+
 
 
 
