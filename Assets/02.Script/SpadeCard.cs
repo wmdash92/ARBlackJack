9d3cf21ab09212ea3e9f44a0f9de4ac862a71281
wmdash92 <wmdash92@gmail.com>
2021-05-10 10:14:52 +0900

    Card Open and Close

--- Assets/02.Script/SpadeCard.cs
+++ Assets/02.Script/SpadeCard.cs
@@ -2,8 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TouchMgr : MonoBehaviour
+
+
+
+
+
+public class SpadeCard : MonoBehaviour
 {
+
+
 
 
 
@@ -12,17 +19,20 @@ public class TouchMgr : MonoBehaviour
 
     public GameObject FairyEffect;
 
-    public Transform CardTr1;
 
-    public GameObject Close;
-    public GameObject OpenSpade;
+    public GameObject Card;
 
 
-    // Start is called before the first frame update
+
+
+
+
     void Start()
     {
         
     }
+
+
 
 
 
@@ -38,40 +48,25 @@ public class TouchMgr : MonoBehaviour
 
         if(Physics.Raycast(ray, out hit, 10.0f, 1<<6))
         {
-
             Debug.Log("hello");
 
-            if (Input.GetMouseButtonDown(0))
-            {Debug.Log("touch");
-            GameObject obj = Instantiate(FairyEffect, CardTr1.transform.position, CardTr1.transform.rotation);
-            Destroy(obj, 3.0f);
 
-        }
-
-
-        }
-
-
-
-
-
-        if(Physics.Raycast(ray, out hit, 10.0f, 1<<7))
-        {
 
             if (Input.GetMouseButtonDown(0))
             {
+
             Debug.Log("touch");
             
-            Close.SetActive(false);
-            OpenSpade.SetActive(true);
+            Card.SetActive(false);
+
+
             }
 
-        }
 
+        }
 
 
 
 
     }
-
 }
