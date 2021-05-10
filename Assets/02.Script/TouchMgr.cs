d26b2d4b19320a8a279ef4291dc254e6ea13f162
wmdash92 <wmdash92@gmail.com>
2021-05-10 14:13:35 +0900

    Card Open PlusEffect bugfix Spade04

--- Assets/02.Script/TouchMgr.cs
+++ Assets/02.Script/TouchMgr.cs
@@ -11,14 +11,13 @@ public class TouchMgr : MonoBehaviour
     private RaycastHit hit;
 
     public GameObject FairyEffect;
+    public GameObject PlasEffect;
 
-    public Transform CardTr1;
+    public Transform Spade02Tr;
+    public Transform Spade03Tr;
+    public Transform Spade04Tr;
 
-    public GameObject Close;
-    public GameObject OpenSpade;
-    public GameObject Back;
-    public GameObject Spade02D;
-    
+
 
 
 
@@ -27,7 +26,7 @@ public class TouchMgr : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
 
         // GameObject Spade02C = GameObject.FindGameObjectWithTag("CARD");
 
@@ -43,21 +42,22 @@ public class TouchMgr : MonoBehaviour
 
     void Update()
     {
-        
+
         ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         Debug.DrawRay(ray.origin, ray.direction * 10.0f, Color.green);
 
 
 
-        if(Physics.Raycast(ray, out hit, 10.0f, 1<<6))
+        if (Physics.Raycast(ray, out hit, 10.0f, 1 << 6))
         {
 
             Debug.Log("hello");
 
             if (Input.GetMouseButtonDown(0))
-            {Debug.Log("touch");
-            GameObject obj = Instantiate(FairyEffect, CardTr1.transform.position, CardTr1.transform.rotation);
-            Destroy(obj, 3.0f);
+            {
+                Debug.Log("touch");
+                GameObject obj = Instantiate(FairyEffect, Spade02Tr.transform.position, Spade02Tr.transform.rotation);
+                Destroy(obj, 3.0f);
 
         }
 
@@ -68,31 +68,114 @@ public class TouchMgr : MonoBehaviour
 
 
 
-        if(Physics.Raycast(ray, out hit, 10.0f, 1<<7))
+        if (Physics.Raycast(ray, out hit, 10.0f, 1 << 7))
         {
 
             if (Input.GetMouseButtonDown(0))
             {
-            
-            Close.SetActive(false);
-            OpenSpade.SetActive(true);
+
+
             }
 
         }
 
 
-        if(Physics.Raycast(ray, out hit, 10.0f, 1<<8))
+
+
+
+        if (Physics.Raycast(ray, out hit, 10.0f, 1 << 8))
         {
 
             if (Input.GetMouseButtonDown(0))
             {
-            
-            GameObject Spade02D = GameObject.Find("Spade02");
-            Spade02D.SetActive(false);
+
+                GameObject BackColor_Black_Spade02 = GameObject.Find("BackColor_Black_Spade02");
+                BackColor_Black_Spade02.SetActive(false);
+
+                //GameObject Spade02 = GameObject.Find("Spade02_Earth").transform.Find("Spade02").gameObject;
+
+                GameObject Spade02 = GameObject.Find("Spade02_Earth");
+                var cards = Spade02.GetComponentsInChildren<Transform>(true);
+                foreach (var card in cards)
+                {
+                    if (card.gameObject.name == "Spade02")
+                    {
+                        card.gameObject.SetActive(true);
+                        GameObject obj = Instantiate(PlasEffect, Spade02Tr.transform.position, Spade02Tr.transform.rotation);
+                        Destroy(obj, 5.0f);
+                        break;
+                    }
+                }
 
             }
-
         }
+
+
+
+
+
+
+
+
+
+
+
+        if (Physics.Raycast(ray, out hit, 10.0f, 1 << 9))
+        {
+
+            if (Input.GetMouseButtonDown(0))
+            {
+
+                GameObject BackColor_Black_Spade03 = GameObject.Find("BackColor_Black_Spade03");
+                BackColor_Black_Spade03.SetActive(false);
+
+
+                GameObject Spade03 = GameObject.Find("Spade03_Earth");
+                var cards = Spade03.GetComponentsInChildren<Transform>(true);
+                foreach (var card in cards)
+                {
+                    if (card.gameObject.name == "Spade03")
+                    {
+                        card.gameObject.SetActive(true);
+                        GameObject effect = Instantiate(FairyEffect, Spade03Tr.transform.position, Spade03Tr.transform.rotation);
+                        Destroy(effect, 5.0f);
+                        break;
+                    }
+                }
+
+            }
+        }
+
+
+
+        if (Physics.Raycast(ray, out hit, 10.0f, 1 << 10))
+        {
+
+            if (Input.GetMouseButtonDown(0))
+            {
+
+                GameObject BackColor_Black_Spade04 = GameObject.Find("BackColor_Black_Spade04");
+                BackColor_Black_Spade04.SetActive(false);
+
+
+                GameObject Spade04 = GameObject.Find("Sun_Spade04");
+                var cards = Spade04.GetComponentsInChildren<Transform>(true);
+                foreach (var card in cards)
+                {
+                    if (card.gameObject.name == "Spade04")
+                    {
+                        card.gameObject.SetActive(true);
+                        GameObject effect = Instantiate(FairyEffect, Spade04Tr.transform.position, Spade04Tr.transform.rotation);
+                        Destroy(effect, 5.0f);
+                        break;
+                    }
+                }
+
+            }
+        }
+
+
+
 
 
 
